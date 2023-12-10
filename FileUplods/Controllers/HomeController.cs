9f149c066d247dcdde574337834a78d94401a90c
Niranjanpaswan1511 <Niranjanpaswan1511@gmail.com>
2023-12-10 18:04:28 +0530

    Add project files.

---
+++ FileUplods/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileUplods.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+
+
+
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult Index(HttpPostedFileBase postedFile)
+        {
+            string filename=Path.GetFileName(postedFile.FileName);
+            string filepath = "!/Uploads/" + filename;
+            postedFile.SaveAs(Server.MapPath(filepath));
+
+            return RedirectToAction("Index");
+
+        }
+    }
+}
